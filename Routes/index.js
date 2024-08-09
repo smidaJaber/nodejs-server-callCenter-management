c889996192780fe42569842e168941efa2edda28
smidaJaber <131284054+smidaJaber@users.noreply.github.com>
2024-08-09 01:47:22 +0100

    Create index.js

---
+++ Routes/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const DB = require("../DB");
+
+const routerTypeProduit = require("./typeproduits");
+const routerDepense = require("./depenses");
+
+const router = express.Router();
+
+module.exports = router;
+
+//clients
